ee22362bd8adcb2d205bedc2532e0c01ab46b97d
youcanalsocode <nehan.gupta12@gmail.com>
2024-05-28 15:57:34 +0530

    correction

--- routes/auth.js
+++ routes/auth.js
@@ -3,7 +3,7 @@ const router = express.Router();
 const User = require("../models/User");
 const { body, validationResult } = require("express-validator");
 const jwt = require("jsonwebtoken");
-const bcrypt = require("bcrypt js");
+const bcrypt = require("bcryptjs");
 const jwt_secret = "itsgood";
 const fetchuser = require("../Middleware/fetchuser");
 
