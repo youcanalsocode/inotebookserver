b86a4a24243f7eb11fe479c2d987dc877a15eaa8
youcanalsocode <nehan.gupta12@gmail.com>
2024-05-28 16:25:32 +0530

    db.js changes

--- db.js
+++ db.js
@@ -3,10 +3,20 @@ const mongoose = require("mongoose");
 const mongoURI = "mongodb://localhost:27017/iNotebooks";
 
 async function connectToMongo() {
-  await mongoose
-    .connect(mongoURI)
-    .then(() => console.log("Connected to Mongo Successfully"))
-    .catch((err) => console.log(err));
+  try {
+    await mongoose.connect(mongoURI, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+    });
+
+    // Wait until the connection is ready
+    await mongoose.connection.readyState;
+
+    console.log("Connected to Mongo Successfully");
+  } catch (error) {
+    console.error("MongoDB connection error:", error.message);
+    process.exit(1); // Exit process with failure
+  }
 }
 
 module.exports = connectToMongo;
