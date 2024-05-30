3dcfd511281e728d52cefc7e774a08d2c3298e8d
youcanalsocode <nehan.gupta12@gmail.com>
2024-05-30 11:31:43 +0530

    adding env file

--- index.js
+++ index.js
@@ -1,9 +1,10 @@
+require("dotenv").config();
 const connectToMongo = require("./db");
 connectToMongo();
 const cors = require("cors");
 const express = require("express");
 const app = express();
-const port = 5000;
+const port = process.env.PORT || 5000;
 app.use(cors());
 
 app.use(express.json());
